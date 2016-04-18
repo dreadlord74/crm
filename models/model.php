8faa4138fa3bc0b4b96fae007e1dfb4d4b9971b2
dreadlord <dreadlord74@yandex.ru>
2016-04-18 14:34:51 +0600

    Теперь соединение с БД создается в модели

--- models/model.php
+++ models/model.php
@@ -7,6 +7,7 @@ defined ("SCRIPT") or die ("Сюда нельзя!");
 session_start();
 
 require_once ("classes/data_base.php");
-
+$mysqli = new data_base();
 
 require_once ("classes/worker.php");
+require_once ("classes/departament.php");
