d618a5aeaddc7abb5728b209ded609a37ca09386
dreadlord <dreadlord74@yandex.ru>
2016-04-18 14:21:10 +0600

    Основная модель, где будут подключаться классы

---
+++ models/model.php
@@ -0,0 +1,12 @@
+<?php
+
+defined ("SCRIPT") or die ("Сюда нельзя!");
+/**
+ * Модель системы
+ */
+session_start();
+
+require_once ("classes/data_base.php");
+
+
+require_once ("classes/worker.php");
