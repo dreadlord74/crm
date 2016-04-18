5507219a805eb0ab9f86058cf61013110de8ef68
dreadlord <dreadlord74@yandex.ru>
2016-04-18 14:35:59 +0600

    Добавлен класс для работы с отделами

--- models/classes/departament.php
+++ models/classes/departament.php
@@ -1,7 +1,30 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: sp
+ * User: Данил Хандысь
  * Date: 18.04.2016
  * Time: 13:25
  */
+
+class departament{
+    public $id;
+    public $name;
+
+    private $db;
+
+   function __construct (){
+       global $mysqli;
+
+       $this->db = $mysqli;
+   }
+
+
+    public function get_all(){
+
+
+        $query = "SELECT * FROM departament";
+
+        return $this->db->super_query($query, true);
+
+    }
+}
