b75ae29cef66a81325bc2e6d263efd1b2ddcbb73
dreadlord <dreadlord74@yandex.ru>
2016-04-18 14:21:34 +0600

    Начата разработка класса работника

--- models/classes/worker.php
+++ models/classes/worker.php
@@ -1,7 +1,20 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: sp
+ * User: Данил Хандысь
  * Date: 18.04.2016
  * Time: 12:33
  */
+
+defined("SCRIPT") or die;
+
+class worker {
+    public $id;
+    public $color;
+
+    private $dep_id;
+
+    private $post_id;
+
+
+}
