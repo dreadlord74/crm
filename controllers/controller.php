a124fe7fc1582311264e7e27a28c8a74bd8aecda
dreadlord <dreadlord74@yandex.ru>
2016-04-18 15:35:31 +0600

    Добавленно подключение контроллера отделов

--- controllers/controller.php
+++ controllers/controller.php
@@ -4,13 +4,27 @@ defined("SCRIPT") or die;
 
 require_once MODELS;
 
+//разбирает массив гет и создает переменные
+
+
 $view = ($_GET['view'] ? $_GET['view'] : "mainTable");
 
+$do = ($_GET['do'] ? $_GET['do'] : "nothing");
+
 switch ($view){
+
+    case "dep":
+        echo 123;
+        require_once CONTROLLERS."departament.php";
+
+        break;
 
     case "mainTable":
 
         break;
+
+    default:
+        $view = "mainTable";
 }
 
 require_once (VIEW."/index.php");
