b2ad907d276e5ba2c62c831bdeb4501349ed65f0
dreadlord <dreadlord74@yandex.ru>
2016-04-19 09:54:35 +0600

    Подключил дополнительные классы

--- models/model.php
+++ models/model.php
@@ -9,5 +9,8 @@ session_start();
 require_once ("classes/data_base.php");
 $mysqli = new data_base();
 
+require_once ("interfaces.php");
+require_once ("classes/abstract_class.php");
 require_once ("classes/worker.php");
 require_once ("classes/departament.php");
+require_once("classes/client.php");
