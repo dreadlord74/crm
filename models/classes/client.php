eecd5c870919033461f89169e07b5c4849466f35
dreadlord <dreadlord74@yandex.ru>
2016-04-19 09:50:28 +0600

    Класс для работы с клиентом

--- models/classes/client.php
+++ models/classes/client.php
@@ -6,7 +6,20 @@
  * Date: 18.04.2016
  * Time: 15:52
  */
-class client
+class client extends abstract_class implements base
 {
+    public function get_name_by_id($id)
+    {
+        // TODO: Implement get_name_by_id() method.
+        $name = $this->db->super_query("SELECT name FROM clients WHERE id=".$id, false);
 
+        return $name[name];
+    }
+
+    public function get_all()
+    {
+        // TODO: Implement get_all() method.
+
+        return $this->db->super_query("SELECT * FROM clients");
+    }
 }
