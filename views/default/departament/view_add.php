497b8255fb054eabc75cc974cac749694d30e9d2
dreadlord <dreadlord74@yandex.ru>
2016-04-19 09:54:46 +0600

    123

---
+++ views/default/departament/view_add.php
@@ -0,0 +1,13 @@
+<?php
+
+defined("SCRIPT") or die();
+
+?>
+
+<div class="center-block">
+    <form class="form-horizontal">
+        <input name="name" placeholder="Название" />
+
+        <input type="submit" name="submit" class="input-group-btn" value="Добавить" />
+    </form>
+</div>
