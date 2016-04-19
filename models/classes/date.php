88398c86db3d9bb731327cfc691fb32ad946baba
dreadlord <dreadlord74@yandex.ru>
2016-04-19 18:15:04 +0600

    Добавлен метод получения всех дат

--- models/classes/date.php
+++ models/classes/date.php
@@ -2,11 +2,14 @@
 
 /**
  * Created by PhpStorm.
- * User: sp
+ * User: Данил Хандысь
  * Date: 19.04.2016
  * Time: 11:18
+ * Класс для работы с таблицой dates
  */
-class date
+class date extends abstract_class
 {
-
+    public function get_all(){
+        return $this->db->super_query("SELECT * FROM dates");
+    }
 }
