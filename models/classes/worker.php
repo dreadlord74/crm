6c9190ec2394610782a9a6d4254c7ff7f7941220
dreadlord <dreadlord74@yandex.ru>
2016-04-19 09:55:27 +0600

    Теперь класс наслудует конструктор и базовый интерфейс

--- models/classes/worker.php
+++ models/classes/worker.php
@@ -8,7 +8,7 @@
 
 defined("SCRIPT") or die;
 
-class worker {
+class worker extends abstract_class implements base {
     public $id;
     public $color;
 
@@ -16,5 +16,23 @@ class worker {
 
     private $post_id;
 
+    public function get_name_by_id($id)
+    {
+        // TODO: Implement get_name_by_id() method.
 
+        $name = $this->db->super_query("SELECT name FROM workers WHERE id=".$id, false);
+
+        return $name[name];
+    }
+
+    public function get_all()
+    {
+        // TODO: Implement get_all() method.
+
+        return $this->db->super_query("SELECT * FROM workers");
+    }
+
+    public function get_by_dep_id($id){
+        return $this->db->super_query("SELECT * FROM workers WHERE departament=".$id);
+    }
 }
