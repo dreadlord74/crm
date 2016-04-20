3417a9b7425a1565461968676fd48063429210fc
dreadlord <dreadlord74@yandex.ru>
2016-04-20 18:00:04 +0600

    Добавлен контроллер работников

--- controllers/worker.php
+++ controllers/worker.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sp
- * Date: 20.04.2016
- * Time: 11:23
- */
+
+defined("SCRIPT") or die;
+
+$worker = new worker();
+
+$dep = new departament();
+
+switch ($do){
+
+    case "add":
+
+        if ($_POST[submit]){
+            $worker->add($_POST['mname'], $_POST[dep]);
+            unset($_POST[submit]);
+        }
+
+        $view = "worker/view";
+
+        break;
+
+    case "del":
+
+        echo $worker->delete($_POST[id]);
+
+        exit();
+        break;
+
+    case "write":
+
+        echo $worker->write($_POST[id], $_POST[name], $_POST[prior]);
+
+        exit();
+        break;
+
+    default:
+
+        $view = "worker/view";
+
+        break;
+}
