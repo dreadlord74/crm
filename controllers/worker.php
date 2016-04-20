220311190f30edcb7023cfe365931ad963316cab
dreadlord <dreadlord74@yandex.ru>
2016-04-20 17:52:36 +0600

    Изменена константа PATH на корректное значение

---
+++ controllers/worker.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sp
+ * Date: 20.04.2016
+ * Time: 11:23
+ */
--- views/default/worker/view.php
+++ views/default/worker/view.php
@@ -1,12 +1,7 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: sp
- * Date: 19.04.2016
+ * Date: 20.04.2016
  * Time: 11:25
  */
-class work
-{
-
-}
