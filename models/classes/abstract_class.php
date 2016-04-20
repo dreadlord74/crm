be423ce9b6e5111f0b019c01a85dbf6c9b68c514
dreadlord <dreadlord74@yandex.ru>
2016-04-20 17:51:26 +0600

    Добавил комментарии и абстрактный метод, изменяющий приоритеты

--- models/classes/abstract_class.php
+++ models/classes/abstract_class.php
@@ -5,10 +5,22 @@ defined("SCRIPT") or die;
 abstract class abstract_class{
     protected $db;
 
+
+    /**
+     * abstract_class constructor.
+     * Вносит объект БД в класс
+     */
     function __construct()
     {
         global $mysqli;
 
         $this->db = $mysqli;
     }
+
+    /**
+     * @param $id
+     * @param $priority
+     * Изменяет приоритет объекта
+     */
+    private function change_priority($id, $priority){}
 }
