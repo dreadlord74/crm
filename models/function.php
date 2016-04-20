080eceac7ea35d1268ed0a55af414a4601040c28
dreadlord <dreadlord74@yandex.ru>
2016-04-20 17:50:44 +0600

    Добавил функцию преобразования даты

--- models/function.php
+++ models/function.php
@@ -1,7 +1,14 @@
 <?php
 
-/*
-function disassemble(&$mass){
-    foreach($mass as $key => $value)
-        ${$key} = $value;
-}*/
+/**
+ * @param $date
+ * @param string $descriptor
+ * @return string
+ * Преобразует дату в читаемый вид
+ */
+function change_date_view($date, $delimiter = "/"){
+
+    $date = explode("-", $date);
+
+    return $date[2].$delimiter.$date[1].$delimiter.$date[0];
+}
