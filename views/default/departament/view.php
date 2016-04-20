57d8484dabfa37b4586b2f3c0328391dbcea69e8
dreadlord <dreadlord74@yandex.ru>
2016-04-20 17:59:28 +0600

    Изменено отображение отделов

--- views/default/departament/view.php
+++ views/default/departament/view.php
@@ -1,8 +1,9 @@
 <script type="text/javascript">
     $(document).ready(function(){
+        var flag = 0;
         function write_click(e){
             $(e).click(function() {
-                $(e).prev("input").prop("disabled", false);
+                $(e).prev("input").prop("disabled", false).prev("input").prop("disabled", false);
                 $(e).attr("src", "<?=VIEW?>/img/sign.png").attr("action", "write_ok");
                 $(e).unbind(write_click(e));
                 $(e).bind(write_ok(e));
@@ -12,44 +13,47 @@
         function write_ok(e){
             $(e).click(function(){
                 $(e).attr("action", "write").attr("src", "<?=VIEW?>/img/tool.png");
-                $(e).prev("input").prop("disabled", true);
+                $(e).prev("input").prop("disabled", true).prev("input").prop("disabled", true);
                 $(e).unbind(write_ok(e));
                 $(e).bind(write_click(e));
 
-                var data = "id="+$(e).prev("input").attr("data-id")+"&name="+$(e).prev("input").val();
-
+                var data = "id="+$(e).prev("input[name=name]").attr("data-id")+"&name="+$(e).prev("input[name=name]").val()+"&prior="+$(e).prev("input[name=name]").prev("input").val();
+                //alert(data);
                 $.ajax({
                     url: "./?view=dep&do=write",
                     type: "POST",
-                    data: data/*,
-                    success: function(data) {
-                        alert(data);
-                        if (data != 0) {
-                            alert("Успех");
-                        } else {
-                            alert("Провал");
-                        }
-                    }*/
+                    data: data,
+                    success: function() {
+                        document.location.href = "<?=PATH.$_SERVER[REQUEST_URI]?>";
+                    }
                 });
 
             });
         }
 
         $("img[action=write]").click(function(){
-            write_click(this);
+            if (flag == 0){
+                write_click(this);
+                $(this).prev("input").prop("disabled", false).prev("input").prop("disabled", false);
+                $(this).attr("src", "<?=VIEW?>/img/sign.png").attr("action", "write_ok");
+                $(this).unbind(write_click(this));
+                $(this).bind(write_ok(this));
+                flag == 1;
+            }
+
         });
 
         $("img[action=del]").click(function(){
             var act = confirm("Вы действительно хотите удалить отдел "+$(this).parent("li").children("input").val()+"?"), li = $(this).parent("li");
 
             if (act){
-                var data = "id="+$(this).parent("li").children("input").attr("data-id");
+                var data = "id="+$(this).parent("li").children("input[name=name]").attr("data-id");
                 $.ajax({
                     url: "./?view=dep&do=del",
                     type: "POST",
                     data: data,
                     success: function(data){
-                        alert(data);
+                        //alert(data);
                         if (data == 1) {
                             alert("Отдел удалён из базы данных!");
                             li.css({display: "none"});
@@ -70,13 +74,14 @@
                     type: "POST",
                     data: "name="+name,
                     success: function (data){
-                        alert(data);
+                       // alert(data);
                         if (data != 0){
                            var el = $.parseJSON(data);
-                            alert(data);
+                            //alert(data);
                            // $.each(data, function(i, el) {
                                 $(".input-group").append('' +
                                     '<li>' +
+                                    '<input style="width: 20px; text-align: center" name="priority" type="text" value="'+el.priority+'" disabled />' +
                                     '<input data-id="' + el.id + '" type="text" name="name" value="' + el.name + '" disabled />' +
                                     '<img action="write" class="img-thumbnail control" src="<?=VIEW?>/img/tool.png" />' +
                                     '<img action="del" class="img-thumbnail control" src="<?=VIEW?>/img/delete.png" />' +
@@ -117,18 +122,19 @@
 </script>
 <div class="row center-block">
     <div class="col-lg-10">
-        <h1>Отделы</h1>
+        <h1><?=$title?></h1>
         <div class="col-lg-4">
             <ol class="list-inline input-group">
                 <?foreach($dep->get_all() as $dep):?>
                     <li>
+                        <input style="width: 20px; text-align: center" name="priority" type="text" value="<?=$dep[priority]?>" disabled />
                         <input data-id="<?=$dep[id]?>" type="text" name="name" value="<?=$dep[name]?>" disabled />
                         <img action="write" class="img-thumbnail control" src="<?=VIEW?>/img/tool.png" />
                         <img action="del" class="img-thumbnail control" src="<?=VIEW?>/img/delete.png" />
                     </li>
                 <?endforeach?>
             </ol>
-            <input style="width: 260px" type="button" class="btn-block" value="Добавить отдел" />
+            <input style="width: 283px" type="button" class="btn-block" value="Добавить отдел" />
         </div>
     </div>
 </div>
