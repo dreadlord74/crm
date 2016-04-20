22b94b3b51364242698a56573098a83608d8f819
dreadlord <dreadlord74@yandex.ru>
2016-04-20 10:00:37 +0600

    Класс основной таблицы

--- models/classes/workTable.php
+++ models/classes/workTable.php
@@ -2,11 +2,13 @@
 
 /**
  * Created by PhpStorm.
- * User: sp
+ * User: Данил Хандысь
  * Date: 19.04.2016
  * Time: 11:24
  */
-class workTable
+class workTable extends abstract_class
 {
-
+    public function get_work_by_worker_id_date_id($w_id, $d_id){
+        return $this->db->super_query("SELECT * FROM work_table WHERE date_id=".$d_id." AND worker_id=".$w_id);
+    }
 }
