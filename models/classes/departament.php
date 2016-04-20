bf9de6d08ef96651e0932e30650708fda54c9b09
dreadlord <dreadlord74@yandex.ru>
2016-04-20 09:50:36 +0600

    Добавлены методы записи и удаления отдела, изменен метод добавления

--- models/classes/departament.php
+++ models/classes/departament.php
@@ -21,17 +21,46 @@ class departament extends abstract_class implements base{
 
     public function get_all(){
 
+        return $this->db->super_query("SELECT * FROM departaments ORDER BY priority");
 
-        $query = "SELECT * FROM departments";
+    }
 
-        return $this->db->super_query($query);
+    public function get_cols_by_id($id){
 
     }
 
     public function add($name){
-        $query = "INSERT INTO departament ('name') VALUES ".$name;
+        $query = "INSERT INTO departaments (name) VALUES ('".$name."')";
 
-        $this->db->query($query);
-        return $this;
+        $id = $this->db->query($query)->get_last_id();
+
+        $name = $this->get_name_by_id($id);
+
+        $mass = array(
+            "id" => $id,
+            "name" => $name
+        );
+
+        return $mass;
+    }
+
+    public function write($id, $name){
+        $result = $this->db->query("UPDATE departaments SET name='".$name."' WHERE id=".$id)->affected();
+
+        if ($result)
+            return 1;
+        else
+            return 0;
+    }
+
+    public function delete($id){
+        $result = $this->db->query("DELETE FROM departaments WHERE id=".$id)->affected();
+
+        /*if ($result)
+            return 1;
+        else
+            return 0;*/
+
+        return $result;
     }
 }
