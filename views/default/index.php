583866321a6ff82da7ccefd76442d066836b1db3
dreadlord <dreadlord74@yandex.ru>
2016-04-20 17:57:04 +0600

    Изменены ссылки в меню

--- views/default/index.php
+++ views/default/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title><?=TITLE." - ".$title?></title>
+    <title><?=TITLE.($title ? " - ".$title : "")?></title>
     <link href="<?=VIEW?>/css/bootstrap.css" rel="stylesheet" />
     <link href="<?=VIEW?>/bootstrap.css" rel="stylesheet" />
     <link href="<?=VIEW?>/css/style.css" rel="stylesheet" />
@@ -17,13 +17,13 @@
     require_once ($view.".php");
 
 ?>
-<div class="row menu">
-    <div class="col-lg-10">
-        <ul class="center-block list-inline">
+<div class="row menu table-bordered">
+    <div class="col-md-12">
+        <ul class="list-inline">
             <li><a href="/">Основная таблица</a> </li>
             <li><a href="/?view=dep">Отделы</a> </li>
+            <li><a href="/?view=worker">Работники</a> </li>
             <li><a href="/?view=client">Клиенты</a> </li>
-            <li><a href="/?view=dep">Работники</a> </li>
         </ul>
     </div>
 </div>
