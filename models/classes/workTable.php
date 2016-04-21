779c1a142b215dd39a8d16fce1fecd6343ba7831
dreadlord <dreadlord74@yandex.ru>
2016-04-21 18:06:41 +0600

    Добавлены методы изменения времени и описания

--- models/classes/workTable.php
+++ models/classes/workTable.php
@@ -11,4 +11,12 @@ class workTable extends abstract_class
     public function get_work_by_worker_id_date_id($w_id, $d_id){
         return $this->db->super_query("SELECT * FROM work_table WHERE date_id=".$d_id." AND worker_id=".$w_id);
     }
+
+    public function write_desc($id, $desc){
+        return $this->db->query("UPDATE work_table SET description='$desc' WHERE id=$id")->affected();
+    }
+
+    public function write_time($id, $time){
+        return $this->db->query("UPDATE work_table SET time='$time' WHERE id=$id")->affected();
+    }
 }
