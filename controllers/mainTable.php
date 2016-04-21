ea1aaae8f5d902057f1b6712e8e1d5294c6c3932
dreadlord <dreadlord74@yandex.ru>
2016-04-21 18:07:31 +0600

    Добавил вызов методов изменения описания и времени

--- controllers/mainTable.php
+++ controllers/mainTable.php
@@ -13,3 +13,22 @@ $date = new date();
 $main = new workTable();
 
 $main_table = $mysqli->super_query("SELECT * FROM work_table");
+
+switch ($do){
+
+    case "write_desc":
+
+        echo $main->write_desc($_POST[id], $_POST[description]);
+
+        exit();
+        break;
+
+    case "write_time":
+        echo $main->write_time($_POST[id], $_POST[time]);
+
+        exit();
+        break;
+    default:
+
+        break;
+}
