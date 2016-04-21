70cd9dca9721090b0a06b8b73e923b2671dd94c0
dreadlord <dreadlord74@yandex.ru>
2016-04-21 18:05:01 +0600

    Контроллер клиента

--- controllers/client.php
+++ controllers/client.php
@@ -6,7 +6,19 @@ $client = new client();
 
 switch ($do){
     case "add":
+        if ($_POST[submit]){
+            $client->add($_POST[name], $_POST[way], $_POST[work_type], $_POST[color], $_POST[text_color], $_POST[date], $_POST[contract_number]);
+            unset($_POST[submit]);
+        }
 
+        $view = "client/view";
+        break;
+
+    case "write":
+
+        echo $client->write($_POST[id], $_POST[name], $_POST[way], $_POST[work_type], $_POST[color], $_POST[text_color], $_POST[date], $_POST[contract_number]);
+
+        exit();
         break;
 
     default:
