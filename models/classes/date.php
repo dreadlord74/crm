ab3cd7dff5d5a063d1a666b87ac473946e32a6ef
dreadlord <dreadlord74@yandex.ru>
2016-04-22 13:45:35 +0600

    ДОбавлены методы добавления даты и получения списка дат по id

--- models/classes/date.php
+++ models/classes/date.php
@@ -12,4 +12,16 @@ class date extends abstract_class
     public function get_all(){
         return $this->db->super_query("SELECT * FROM dates");
     }
+
+    public function add($date){
+        $is_work = ((get_day_of_week($date) == "вс" || get_day_of_week($date) == "сб") ? "0" : "1");
+
+        return $this->db->query("INSERT INTO dates (date, is_work_day) VALUES ('$date', '$is_work')")->get_last_id();
+    }
+
+    public function get_by_ids($ids){
+         if (count($ids) != 0) {
+             return $this->db->super_query("SELECT * FROM dates WHERE id IN (" . implode(", ", $ids) . ")");
+         }
+    }
 }
