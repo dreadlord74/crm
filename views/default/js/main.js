ed1951ab69e8fdc2c7079e1bd317fa22008ea294
dreadlord <dreadlord74@yandex.ru>
2016-04-22 10:04:52 +0600

    Скрипт изменения данных в таблице

---
+++ views/default/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(e) {
+	var text, time;
+	
+	$(".td > textarea").hover(function(){
+		$(this).parent("td").attr("delay", 1);
+	}, function(){
+		var obj = $(this).parent("td");
+		
+		obj.attr("delay", 0);
+	});
+	
+    $(".td").hover(function(){
+			
+			text = $(this).children("textarea").val();
+			
+			$(this).children("textarea").css({display: "block"});
+			
+		}, function(){
+			var obj = $(this);
+			setTimeout(function(){
+				if (obj.attr("delay") == 0)
+					obj.children("textarea").css({display: "none"});
+			}, 1000);
+			
+			if (text != $(this).children("textarea").val()){
+				var data = "description="+$(this).children("textarea").val()+"&id="+$(this).attr("work-id");
+				$.ajax({
+					url: "./?view=mainTable&do=write_desc",
+					type: "POST",
+					data: data,
+					success: function(data){
+						if (data == 0)
+							alert("Не удалось изменить текстовое поле.");
+					}
+				});
+			}			
+		}
+	);
+	
+	$(".td").next("td > input").children("input").on("focusin", function(){
+		time = $(this).val();
+	});
+	
+	$(".td").next("td").children("input").on("focusout", function(){
+		if (time != $(this).val()){
+			var data = "time="+$(this).val()+"&id="+$(this).attr("work-id");
+			$.ajax({
+					url: "./?view=mainTable&do=write_time",
+					type: "POST",
+					data: data,
+					success: function(data){
+						if (data == 0)
+							alert("Не удалось изменить время.");
+					}
+				});
+		}
+	});
+	
+	
+	
+});
