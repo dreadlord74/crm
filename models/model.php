e7a5d0e38db8fc6595148529722c5e9089801214
dreadlord <dreadlord74@yandex.ru>
2016-05-18 16:25:51 +0500

    попробовал подставить функцию автозагрузки классов

--- models/model.php
+++ models/model.php
@@ -7,14 +7,23 @@ defined ("SCRIPT") or die ("Сюда нельзя!");
 session_start();
 
 require_once ("function.php");
+require_once ("interfaces.php");
+
+/**
+ * Функция автозагрузки классов - не работает в текущей версии пхп
+ * @param $class - имя класса
+ */
+function __autoload($class){
+    require_once "classes/".$class.".php";
+}
 
 require_once ("classes/data_base.php");
 $mysqli = new data_base();
 
-require_once ("interfaces.php");
+
 require_once ("classes/abstract_class.php");
 require_once ("classes/workTable.php");
 require_once ("classes/worker.php");
+require_once ("classes/client.php");
 require_once ("classes/date.php");
 require_once ("classes/departament.php");
-require_once ("classes/client.php");
