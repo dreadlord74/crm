4051aaa3d165eaa4ead604d43b048f13cc6efd8c
dreadlord <dreadlord74@yandex.ru>
2016-05-18 16:22:02 +0500

    Добавлены несколько новых кейсов

--- controllers/mainTable.php
+++ controllers/mainTable.php
@@ -12,13 +12,18 @@ $date = new date();
 
 $main = new workTable();
 
-$main_table = $mysqli->super_query("SELECT * FROM work_table");
-
 switch ($do){
 
     case "write_desc":
 
         echo $main->write_desc($_POST[id], $_POST[description]);
+
+        exit();
+        break;
+
+    case "write_work":
+
+        echo $main->write_work($_POST[id], $_POST[client_id]);
 
         exit();
         break;
@@ -31,12 +36,42 @@ switch ($do){
 
     case "add_work":
 
-        echo $main->add_work($_POST[date_id], $_POST[dep_id], $_POST[client_id], $_POST[worker_id]);
+        echo json_encode($main->add_work($_POST[date_id], $_POST[dep_id], $_POST[client_id], $_POST[worker_id]));
 
         exit();
         break;
 
+    case "change_text":
+
+        echo $main->change_text($_POST[id], $_POST[date_id], $_POST[dep_id], $_POST[worker_id], $_POST[text]);
+
+        exit();
+        break;
+
+    case "delete":
+
+        echo $main->delete($_POST[id]);
+
+        exit();
+        break;
+	
+	case "add_deadline":
+	
+		echo json_encode($date->add_deadline($_POST[client_id], $_POST[date_id]));
+	
+		exit();
+		break;
+		
+	case "delete_deadline":
+		
+		echo $date->delete_deadline($_POST[id]);
+		
+		exit(); 
+		break;
+
     default:
+	
+		$main_table = $mysqli->super_query("SELECT * FROM work_table");
 
         break;
 }
