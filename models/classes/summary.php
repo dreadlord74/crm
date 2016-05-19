9468b574bd5b8b53145ec5abdbf9afa1ea1f0d8e
dreadlord <dreadlord74@yandex.ru>
2016-05-19 18:05:43 +0500

    Добавлены всякие методы

--- models/classes/summary.php
+++ models/classes/summary.php
@@ -1,12 +1,37 @@
 <?php
 
+defined("SCRIPT") or die("");
+
 /**
  * Created by PhpStorm.
- * User: sp
+ * User: Хандысь Данил
  * Date: 19.05.2016
  * Time: 16:36
  */
-class summary
+class summary extends abstract_class
 {
+    public function get_all()
+    {
+        return $this->db->super_query("SELECT * FROM summary WHERE is_archive='0'");
+    }
 
+    public function get_ids()
+    {
+        return $this->db->super_query("SELECT id FROM summary WHERE is_archive='0'");
+    }
+
+    public function write_e_date(&$id, &$date)
+    {
+        return $this->db->query("UPDATE summary SET end_date='$date' WHERE id=$id")->affected();
+    }
+
+    public function write_b_date(&$id, &$date)
+    {
+        return $this->db->query("UPDATE summary SET begin_date='$date' WHERE id=$id")->affected();
+    }
+
+    public function write_hours(&$id, &$hours)
+    {
+        return $this->db->query("UPDATE summary SET hours=$hours WHERE id=$id")->affected();
+    }
 }
