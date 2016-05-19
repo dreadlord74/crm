bf35dfebea1eddf69954711eb72431d535bfa1e4
dreadlord <dreadlord74@yandex.ru>
2016-05-19 09:58:01 +0500

    начал верстку сводной таблицы

--- views/default/summary/view.php
+++ views/default/summary/view.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sp
- * Date: 18.05.2016
- * Time: 16:13
- */
+<div class="content">
+    <div class="head">
+        <div class="client">
+            Клиент
+        </div>
+        <div class="saled">
+            Продано часов
+        </div>
+        <div class="ostatok">
+            Остаток/Перебор
+        </div>
+        <div class="rol">
+            Роль
+        </div>
+        <div class="plan">
+            Длит. дней
+        </div>
+        <div class="date">
+            Дата окончания
+        </div>
+        <div class="plan hours">
+            План часов
+        </div>
+        <div class="plan fact">
+            Факт часов
+        </div>
+        <div class="prognoz">
+            Прогноз превыш.
+        </div>
+        <div class="ost">
+            Остаток/потери
+        </div>
+        <div class="month">
+            Окт.-15
+        </div>
+    </div>
+</div>
