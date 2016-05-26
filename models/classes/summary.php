7a311155f593ad50db4ca786c746a7ecc4f8894f
dreadlord <dreadlord74@yandex.ru>
2016-05-26 17:46:16 +0500

    Добавлены методы получения суммы часов работ за перечисленные дни по id клиента и работника

--- models/classes/summary.php
+++ models/classes/summary.php
@@ -40,5 +40,35 @@ class summary extends abstract_class
         return $this->db->super_query("SELECT * FROM summary_info WHERE summary_id=$id");
     }
 
+    public function calc_work_by_dates_id(&$dates_id, &$clietn_id, &$worker_id)
+    {
+        $res = $this->db->super_query("SELECT sum(time) as summa FROM work_table WHERE date_id IN(".implode(" ,", $dates_id).")
+                                            AND client_id=$clietn_id AND worker_id=$worker_id",false);
+        return $res[summa];
+    }
 
+    public function delete_info_by_worker_id(&$id)
+    {
+        return $this->db->query("DELETE FROM summary_info WHERE worker_id=$id")->affected();
+    }
+
+    public function write_plan_h(&$sum_id, &$worker_id, &$value)
+    {
+        return $this->db->query("UPDATE summary_info SET plan_hours=$value WHERE summary_id=$sum_id AND worker_id=$worker_id")->affected();
+    }
+
+    public function write_days(&$sum_id, &$worker_id, &$date, &$value, $offset = 0)
+    {
+        $value += $offset;
+
+        $res = $this->db->super_query("SELECT date FROM dates WHERE date>'$date' AND is_work_day!='0' ORDER BY date LIMIT $value");
+
+        $res = array_pop($res);
+
+        $value -= $offset;
+
+        $this->db->query("UPDATE summary_info SET days_count=$value, date_end='{$res[date]}' WHERE summary_id=$sum_id AND worker_id=$worker_id");
+
+        return change_date_view($res[date]);
+    }
 }
