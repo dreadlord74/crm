1568333ab263f302788e857a8cb1fe724836ee4f
dreadlord <dreadlord74@yandex.ru>
2016-05-30 09:35:05 +0500

    Вставил фавикон и изменил текст ссыдки

--- views/default/index.php
+++ views/default/index.php
@@ -5,6 +5,7 @@
     <title><?=TITLE.($title ? " - ".$title : "")?></title>
     <link href="<?=VIEW?>/css/bootstrap.css" rel="stylesheet" />
     <link href="<?=VIEW?>/bootstrap.css" rel="stylesheet" />
+    <link href="data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAADgl08Aaz0PAPz7+gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARIBEiIBIgERIBEiIgEiARIBEiIiASEBIBEiIiIBEBEBEiASAQEQEREiIBASASIBEiIgESIiIiIiIiAREiIiIiIiIBIBIiIBIBEgEgEiIBEBIBAREiIBEgEiIiIiIBEgASIiIiIBEgEBIiIiIBEgEQEgEiIBEgESIBEiIBEgESIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" rel="icon" type="image/x-icon" />
     <link href="<?=VIEW?>/css/style.css" rel="stylesheet" />
     <script type="text/javascript" src="<?=VIEW?>/js/jquery-2.0.0.min.js"></script>
     <script type="text/javascript" src="<?=VIEW?>/js/bootstrap.min.js"></script>
@@ -21,7 +22,7 @@
     <?echo 'Памяти использовано: ',round(memory_get_usage()/1024/1024,2),' MB';?>
     <div class="col-md-12">
         <ul class="list-inline">
-            <li><a href="/">Основная таблица</a> </li>
+            <li><a href="/">План загруженности</a> </li>
             <li><a href="/?view=summary">Сводная таблица</a> </li>
             <li><a href="/?view=dep">Отделы</a> </li>
             <li><a href="/?view=worker">Работники</a> </li>
